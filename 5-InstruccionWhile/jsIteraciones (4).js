75ba4d51379cef9ee87e7c92027a05465c58694a
franccode <franconicolasbordon@gmail.com>
2020-02-18 22:09:59 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,18 @@ function mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	//while(!(numero >= 0 && numero <= 9)){  //a)
+	while (numero < 0 || numero > 9 || isNaN (numero)){ //b) (isNaN = es no numero)
+		numero = parseInt(prompt("Error. Reingrese un numero"));
+	}
+	document.getElementById("Numero").value = numero;
 
 
 }//FIN DE LA FUNCIÓN
+
+//a) mientras el numero no este entre 0 y 9
+//b) mientras el numero sea menor a 0 o mayor a nueve o no sea un numero 
+
+//EJERCIOCIO QUE PIDE CLAVE -> while(!(clave == "utn750"))
+//a) mientras la clave no es utn750
+//b) mientas la clave sea distinta de utn750 
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,16 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+var contador=0;
+var acumulador=0;
 
-
+while (contador < 5){
+var num = parseInt(prompt("Ingrese un numero: "));
+acumulador = acumulador + num;
+contador++;
+}
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value= acumulador/5;
 
 }//FIN DE LA FUNCIÓN
